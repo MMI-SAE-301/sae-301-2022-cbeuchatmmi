786a2d315191f2f8b5c214c10e4de59ca9924529
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-10-25 16:31:53 +0200

    Import color et font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ const formKitTailwind = require("@formkit/themes/tailwindcss");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      "colors": {
+        "noir": "#1e1c1c",
+        "noir2": "#282424",
+        "brun": "#958575",
+        "gris": "#eaeaea",
+        "blanc": "#fafafa",
+        "brun2": "#cbb7a4"
+      },
+      "fontSize": {
+        "base": "1rem",
+        "lg": "2.3125rem",
+        "xl": "3.3125rem",
+        "2xl": "5.3125rem",
+        "3xl": "7.6875rem"
+      },
+      "fontFamily": {
+        "darker-grotesque": "Darker Grotesque",
+        "cantarell": "Cantarell"
+      },
+      "borderRadius": {
+        "none": "0",
+        "xs": "2rem",
+        "sm": "2.5rem",
+        "default": "3.125rem"
+      }
+
+
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
