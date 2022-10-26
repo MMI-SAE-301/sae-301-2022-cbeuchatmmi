d1221afddbfbf7e3b4379c0ec93b09aec9e4097f
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-10-26 21:23:50 +0200

    CSS responsive lg form

--- src/components/FormMontre.vue
+++ src/components/FormMontre.vue
@@ -76,51 +76,55 @@ async function supprimerMontre() {
 }
 </script>
 <template >
-    <div class="text-brun2 font-darker-grotesque text-[2rem]">
+    <div class="text-brun2 font-darker-grotesque text-[2rem] lg:flex lg:justify-between">
+
+        <div class="">
 
 
+            <FormKit type="form" v-model="montre" @submit="upsertMontre" submit-label="Enregistrer">
 
-        <FormKit type="form" v-model="montre" @submit="upsertMontre" submit-label="Enregistrer">
 
+                <FormKitListColors name="boitier" label="Boitier" />
+                <FormKit type="select" name="idmateriaux_boitier" label="Matériaux Bracelet"
+                    :options="optionsMateriaux" />
+                <FormKitListColors name="bracelet" label="Bracelet" />
+                <FormKit type="select" name="idmateriaux_bracelet" label="Matériaux Bracelet"
+                    :options="optionsMateriaux" />
+                <FormKit name="taille" label="Taille" type="select" :options="{
+                    14: '14cm',
+                    15: '15cm',
+                    16: '16cm',
+                    17: '17cm',
+                    18: '18cm',
+                    19: '19cm',
+                }" />
+                <FormKitEcran name="ecran" label="Écran" />
+                <FormKit label="Forme de la montre" name="forme" type="radio" :options="forme"
+                    options-class="flex gap-4" radioDecorator="bg-brun" />
 
-            <FormKitListColors name="boitier" label="Boitier" />
-            <FormKit type="select" name="idmateriaux_boitier" label="Matériaux Bracelet" :options="optionsMateriaux" />
-            <FormKitListColors name="bracelet" label="Bracelet" />
-            <FormKit type="select" name="idmateriaux_bracelet" label="Matériaux Bracelet" :options="optionsMateriaux" />
-            <FormKit name="taille" label="Taille" type="select" :options="{
-                14: '14cm',
-                15: '15cm',
-                16: '16cm',
-                17: '17cm',
-                18: '18cm',
-                19: '19cm',
-            }" />
-            <FormKitEcran name="ecran" label="Écran" />
-            <FormKit label="Forme de la montre" name="forme" type="radio" :options="forme" options-class="flex gap-4"
-                radioDecorator="bg-brun" />
+            </FormKit>
+            <RouterLink to="/montre/indexMontre">
+                <Bouton nom="Vos montres" class="bg-brun mb-2 text-blanc font-darker-grotesque  rounded-xl " />
+            </RouterLink>
 
-        </FormKit>
-        <RouterLink to="/montre/indexMontre">
-            <Bouton nom="Vos montres" class="bg-brun mb-2 text-blanc font-darker-grotesque  rounded-xl " />
-        </RouterLink>
-
-        <button type="button" v-if="montre.idmontre" @click="($refs.dialogSupprimer as any).showModal()"
-            class="focus-style justify-self-end rounded-md bg-red-500 p-2 shadow-sm">
-            Supprimer
-        </button>
-        <dialog ref="dialogSupprimer" @click="($event.currentTarget as any).close()">
-            <button type="button" class="focus-style justify-self-end rounded-md bg-blue-300 p-2 shadow-sm">
-                Annuler</button><button type="button" @click="supprimerMontre()"
-                class="focus-style rounded-md bg-red-500 p-2 shadow-sm">
-                Confirmer suppression
+            <button type="button" v-if="montre.idmontre" @click="($refs.dialogSupprimer as any).showModal()"
+                class="focus-style justify-self-end rounded-md bg-red-500 p-2 shadow-sm">
+                Supprimer
             </button>
-        </dialog>
+            <dialog ref="dialogSupprimer" @click="($event.currentTarget as any).close()">
+                <button type="button" class="focus-style justify-self-end rounded-md bg-blue-300 p-2 shadow-sm">
+                    Annuler</button><button type="button" @click="supprimerMontre()"
+                    class="focus-style rounded-md bg-red-500 p-2 shadow-sm">
+                    Confirmer suppression
+                </button>
+            </dialog>
+
+
+        </div>
 
 
 
-
-
-        <div class="bg-gris flex flex-col items-center justify-center gap-4">
+        <div class="bg-gris flex flex-col items-center justify-center gap-4 lg:w-[600px]   ">
             <MontreCarre class="w-64" v-bind="montre" id="carre" />
             <MontreRonde class=" w-64" v-bind="montre" id="ronde" />
         </div>
