d11d628dcd09d7d4ee38aece4097e86d2068ef33
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-10-26 17:03:38 +0200

    Connection à google et facebook

---
+++ src/pages/Connection.vue
@@ -0,0 +1,19 @@
+
+<script setup lang = "ts" >
+import { supabase, user } from '../supabase'
+</script>
+
+<template>
+    <button @pointerdown="supabase.auth.signIn({ provider: 'google' })">
+        Se connecter avec Google
+    </button>
+    <br />
+
+
+    <button @pointerdown="supabase.auth.signIn({ provider: 'facebook' })">
+        Se connecter avec Facebook
+    </button>
+    <button class="bg-amber-500 rounded-[25px] m-5" v-if="user" @pointerdown="supabase.auth.signOut()">
+        Se déconnecter ({{ user.email }})
+    </button>
+</template>
