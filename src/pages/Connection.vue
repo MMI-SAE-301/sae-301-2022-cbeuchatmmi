9b2f58518cfa0badaeab215c6f5d9bce72a58402
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-10-26 20:53:16 +0200

    CSS connection

--- src/pages/Connection.vue
+++ src/pages/Connection.vue
@@ -4,16 +4,24 @@ import { supabase, user } from '../supabase'
 </script>
 
 <template>
-    <button @pointerdown="supabase.auth.signIn({ provider: 'google' })">
-        Se connecter avec Google
-    </button>
-    <br />
+
+    <h2 class="bg-noir  text-center font-darker-grotesque text-xl text-brun2">Connectez vous</h2>
+    <div class="bg-noir2 flex flex-col items-center justify-center gap-4">
+
+        <button class="border-2 mt-6 border-brun2 text-brun2 p-2 rounded-full"
+            @pointerdown="supabase.auth.signIn({ provider: 'google' })">
+            Se connecter avec Google
+        </button>
+        <br />
 
 
-    <button @pointerdown="supabase.auth.signIn({ provider: 'facebook' })">
-        Se connecter avec Facebook
-    </button>
-    <button class="bg-amber-500 rounded-[25px] m-5" v-if="user" @pointerdown="supabase.auth.signOut()">
-        Se déconnecter ({{ user.email }})
-    </button>
+        <button class="border-2 mb-6 border-brun2 text-brun2 p-2 rounded-full"
+            @pointerdown="supabase.auth.signIn({ provider: 'facebook' })">
+            Se connecter avec Facebook
+        </button>
+        <button class="border-2 mb-6 bg-brun2 text-noir2 p-2 rounded-full" v-if="user"
+            @pointerdown="supabase.auth.signOut()">
+            Se déconnecter ({{ user.email }})
+        </button>
+    </div>
 </template>
