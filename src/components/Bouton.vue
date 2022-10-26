80d20988465760e27bd0ce9e0f192d026eb6f20a
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-10-26 09:12:42 +0200

    composant bouton

---
+++ src/components/Bouton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import chevronBas from '@/components/chevronBas.vue'
+defineProps({
+    nom: String,
+});
+</script>
+<template>
+
+    <button class=" bg-brun text-blanc font-darker-grotesque text-lg p-2 rounded-xl">
+        {{ nom }}
+    </button>
+</template>
