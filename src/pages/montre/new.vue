829c119128f46884b90b76bee37b03fa7412388f
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-10-26 20:43:45 +0200

    css Formulaire "new.vue"

--- src/pages/montre/new.vue
+++ src/pages/montre/new.vue
@@ -5,10 +5,16 @@ import FormMontre from "@/components/FormMontre.vue";
 
 </script>
 
-<template>
+<template >
 
-    <FormMontre />
+    <h1 class="bg-noir2  text-center font-darker-grotesque text-xl text-brun2">Personnaliser</h1>
+    <FormMontre class="bg-noir2" />
+    <h2 class="bg-noir2  text-center font-darker-grotesque text-xl text-brun2"> Styles de Montres</h2>
+    <div class="flex flex-col items-center justify-center gap-4">
 
+        <img class="w-[75%]" src="@/assets/Montre1.png" alt="">
+        <img class="w-[75%]" src="@/assets/Montre2.png" alt="">
+        <img class="w-[75%]" src="@/assets/Montre3.png" alt="">
 
-
+    </div>
 </template>
