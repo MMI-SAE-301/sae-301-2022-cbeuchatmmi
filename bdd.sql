58fb82aeb7e9bdf06f3b635a1ebb2994b5fa0410
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-10-27 14:58:51 +0200

    Màj bdd

--- bdd.sql
+++ bdd.sql
@@ -12,8 +12,7 @@ Forme varchar,
 Forme int,
 idMateriaux_bracelet uuid,
 idMateriaux_boitier uuid,
-idClient_commande uuid,
-idClient_enregistrer uuid,
+idClient uuid,
 Primary Key (idMontre),
 Foreign Key (idMateriaux_bracelet) references Materiaux(idMateriaux),
 Foreign Key (idMateriaux_boitier) references Materiaux(idMateriaux),
@@ -43,39 +42,29 @@ Primary Key (idClient)
 -- code pour la création des vues
 --
 create view listeMontre as
-select "idclient_enregistrer", "idmontre","ecran","bracelet","idmateriaux_bracelet", "boitier", "idmateriaux_boitier"  from "montre";
+select "idclient", "idmontre","ecran","bracelet","idmateriaux_bracelet", "boitier", "idmateriaux_boitier"  from "montre";
 
 create view listeMontreClient as
-select "idclient_enregistrer", "idmontre", "idclient_commande"  from "montre";
+select "idclient", "idmontre", "idclient_commande"  from "montre";
 
 --
 -- code pour la création des policies
 --
 create policy "Les clients peuvent modifier leur montre"
 on montre for update using (
-uid() = idclient
+uid() = user_id
 );
 
 create policy "Les clients peuvent supprimer leur montre"
 on montre for delete using (
-uid() = idclient
+uid() = user_id
 );
 
 create policy "Seul les clients peuvent enregistrer leur montre"
 ON montre FOR INSERT
 TO authenticated
-WITH CHECK (uid () in (Select "idclient" from client));
+WITH CHECK (uid () in (Select "user_id" from user));
 
 
-create policy "Les clients peuvent modifier leur compte"
-on client for update using (
-uid() = idclient
-);
-
-
-create policy "Les clients peuvent supprimer leur compte"
-on client for delete using (
-uid() = idclient
-);
 
 
